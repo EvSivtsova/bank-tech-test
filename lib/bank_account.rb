cac0d6d9c8273ecda73bce07d6632935bb19de49
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-22 19:33:33 +0100

    Update the balance when depositing money

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,13 +1,19 @@
 class BankAccount
   def initialize
-    @transactions = 0
+    @transactions = []
+    @balance = 0
   end
 
-  def deposit(value, date = Time.now());
-    @transactions = [value, date]
+  def deposit(value, date = Time.new().strftime('%d-%m-%Y'));
+    @transactions.push({date: date, value: value})
+    @balance += value
   end
 
   def get_transactions
     return @transactions
+  end
+
+  def get_balance
+    return @balance
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,11 +1,26 @@
 require 'bank_account'
 
 describe BankAccount do
-  it "records and returns the value and the date of the deposit transaction " do
-    bank_account = BankAccount.new
-    bank_account.deposit(1000, '10-01-2023')
-    expect(bank_account.get_transactions).to include 1000
-    expect(bank_account.get_transactions).to include '10-01-2023'
+  context "when depositing money" do
+    it "records and returns the value and the date of the transactions" do
+      bank_account = BankAccount.new
+      bank_account.deposit(1000, '10-01-2023')
+      expect(bank_account.get_transactions[0]).to include({date: '10-01-2023', value: 1000})
+    end
+    
+    it "updates the balance of the account" do
+      bank_account = BankAccount.new
+      bank_account.deposit(1000, '10-01-2023')
+      expect(bank_account.get_balance).to eq 1000
+    end
+    
+    it "updates the balance of the account when several transaction have been made" do
+      time_double = Time.new(2022, 8, 22, 16, 21, 12).strftime('%d-%m-%Y')
+      bank_account = BankAccount.new
+      bank_account.deposit(1000, '10-01-2023')
+      bank_account.deposit(2000, time_double)
+      expect(bank_account.get_balance).to eq 3000
+      expect(bank_account.get_transactions[1]).to include({date: '22-08-2022', value: 2000})
+    end
   end
 end
-  
