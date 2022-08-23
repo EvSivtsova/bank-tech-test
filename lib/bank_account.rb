2009e1d1ca9b1825149807e5782bab73c4fc55fb
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-23 11:22:07 +0100

    Print bank statement in required format

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,25 +1,53 @@
 class BankAccount
-  def initialize
+  def initialize(io)
     @transactions = []
     @balance = 0
+    @io = io
   end
 
-  def deposit(value, date = Time.new().strftime('%d-%m-%Y'));
-    @transactions.push({date: date, value: value})
+  def deposit(value, date = Time.new().strftime('%d/%m/%Y'));
     @balance += value
+    @transactions.push({
+      date: date, 
+      value: value, 
+      balance: @balance})
   end
   
-  def withdraw(value, date = Time.new().strftime('%d-%m-%Y'));
-    value = -(value)
-    @transactions.push({date: date, value: value})
-    @balance += value
+  def withdraw(value, date = Time.new().strftime('%d/%m/%Y'));
+    @balance -= value
+    @transactions.push({
+      date: date, 
+      value: -value, 
+      balance:  @balance})
   end
-
+  
   def get_transactions
     return @transactions
   end
-
+  
   def get_balance
     return @balance
+  end
+
+  def get_bank_statement
+    bank_statement_header
+    bank_statement_body
+  end
+  
+  private 
+  
+  def bank_statement_header
+    @io.puts "date || credit || debit || balance"
+  end
+  
+  def bank_statement_body
+    @transactions.reverse.map do |transaction|
+      if transaction[:value] > 0
+        transaction_value = "|| #{'%.2f' % transaction[:value]} || ||"
+      else
+        transaction_value = "|| || #{'%.2f' % -transaction[:value]} ||"
+      end
+      @io.puts "#{transaction[:date]} #{transaction_value} #{'%.2f' % transaction[:balance] if !transaction[:balance].nil?}"
+    end
   end
 end
