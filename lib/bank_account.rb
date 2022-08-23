0e3ba692764b04da095d3d2719e38402b000f6e9
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-23 13:23:04 +0100

    Lint the code and address the errors

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,61 +1,54 @@
 class BankAccount
+  attr_accessor :transactions, :balance
+
   def initialize(io)
     @transactions = []
     @balance = 0
     @io = io
   end
 
-  def deposit(value, date = Time.new().strftime('%d/%m/%Y'));
+  def deposit(value, date = Time.new.strftime('%d/%m/%Y'))
     @balance += value
-    @transactions.push({
-      date: date, 
-      value: value, 
-      balance: @balance})
-  end
-  
-  def withdraw(value, date = Time.new().strftime('%d/%m/%Y'));
-    @balance -= value
-    @transactions.push({
-      date: date, 
-      value: -value, 
-      balance:  @balance})
-  end
-  
-  def get_transactions
-    return @transactions
-  end
-  
-  def get_balance
-    return @balance
+    @transactions.push({ date: date, value: value, balance: @balance })
   end
 
-  def get_bank_statement
+  def withdraw(value, date = Time.new.strftime('%d/%m/%Y'))
+    @balance -= value
+    @transactions.push({ date: date, value: -value, balance: @balance })
+  end
+
+  def bank_statement
     bank_statement_header
     bank_statement_body
   end
-  
-  private 
-  
+
+  private
+
   def bank_statement_header
-    @io.puts "date || credit || debit || balance"
+    @io.puts 'date || credit || debit || balance'
   end
-  
+
   def bank_statement_body
     @transactions.reverse.map do |transaction|
-      if transaction[:value] > 0
-        transaction_value = "|| #{'%.2f' % transaction[:value]} || ||"
-      else
-        transaction_value = "|| || #{'%.2f' % -transaction[:value]} ||"
-      end
-      @io.puts "#{transaction[:date]} #{transaction_value} #{'%.2f' % transaction[:balance] if !transaction[:balance].nil?}"
+      @io.puts "#{transaction[:date]} #{debit_or_credit(transaction)} #{balance_formatted(transaction)}"
     end
+  end
+
+  def debit_or_credit(transaction)
+    return format('|| %.2f || ||', transaction[:value]) unless transaction[:value].negative?
+
+    format('|| || %.2f ||', -transaction[:value])
+  end
+
+  def balance_formatted(transaction)
+    !transaction[:balance].nil? ? format('%.2f', transaction[:balance]) : '0.00'
   end
 end
 
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
   bank_account = BankAccount.new(Kernel)
   bank_account.deposit(1000, '10/01/2023')
   bank_account.deposit(2000, '13/01/2023')
   bank_account.withdraw(500, '14/01/2023')
-  bank_account.get_bank_statement
- end 
+  bank_account.bank_statement
+end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,61 +1,46 @@
 require 'bank_account'
 
 describe BankAccount do
-  context "when depositing money" do
-    it "records and returns the value and the date of the transactions" do
+  context 'when depositing money' do
+    it 'records and returns the value and the date of the transactions' do
       io = double :io
       bank_account = BankAccount.new(io)
       bank_account.deposit(1000, '10/01/2023')
-      expect(bank_account.get_transactions[0]).to include({date: '10/01/2023', value: 1000, balance: 1000})
+      expect(bank_account.transactions[0]).to include({ date: '10/01/2023', value: 1000, balance: 1000 })
     end
-    
-    it "updates the balance of the account" do
-      io = double :io
-      bank_account = BankAccount.new(io)
-      bank_account.deposit(1000, '10/01/2023')
-      expect(bank_account.get_balance).to eq 1000
-    end
-    
-    it "updates the balance of the account when several transaction have been made" do
+
+    it 'updates the balance of the account when several transaction have been made' do
       time_double = Time.new(2022, 8, 22, 16, 21, 12).strftime('%d/%m/%Y')
       io = double :io
       bank_account = BankAccount.new(io)
       bank_account.deposit(1000, '10/01/2023')
       bank_account.deposit(2000, time_double)
-      expect(bank_account.get_balance).to eq 3000
-      expect(bank_account.get_transactions[1]).to include({date: '22/08/2022', value: 2000, balance: 3000})
+      expect(bank_account.balance).to eq 3000
+      expect(bank_account.transactions[1]).to include({ date: '22/08/2022', value: 2000, balance: 3000 })
     end
   end
-  
-  context "when withdrawing money" do
-    it "records the withdrawal transactions" do
+
+  context 'when withdrawing money' do
+    it 'records the withdrawal transactions' do
       io = double :io
       bank_account = BankAccount.new(io)
       bank_account.withdraw(1000, '10/01/2023')
-      expect(bank_account.get_transactions[0]).to include({date: '10/01/2023', value: -1000, balance: -1000})
+      expect(bank_account.transactions[0]).to include({ date: '10/01/2023', value: -1000, balance: -1000 })
     end
-    
-    it "updates the balance" do
-      io = double :io
-      bank_account = BankAccount.new(io)
-      bank_account.withdraw(1000)
-      expect(bank_account.get_balance).to eq -1000
-    end
-    
-    it "updates the balance when several transaction have been made" do
+
+    it 'updates the balance when several transaction have been made' do
       time_double = Time.new(2022, 8, 23, 16, 21, 12).strftime('%d/%m/%Y')
       io = double :io
       bank_account = BankAccount.new(io)
       bank_account.withdraw(1000)
       bank_account.withdraw(500, time_double)
-      expect(bank_account.get_balance).to eq -1500
-      expect(bank_account.get_transactions[1]).to include({date: '23/08/2022', value: -500, balance: -1500})
+      expect(bank_account.balance).to eq(-1500)
+      expect(bank_account.transactions[1]).to include({ date: '23/08/2022', value: -500, balance: -1500 })
     end
- 
   end
-  
-  context "when both depositing and withdrawing money" do
-    it "records withdrawal and deposit transations" do
+
+  context 'when both depositing and withdrawing money' do
+    it 'records withdrawal and deposit transations' do
       time_double = Time.new(2022, 8, 22, 16, 21, 12).strftime('%d/%m/%Y')
       io = double :io
       bank_account = BankAccount.new(io)
@@ -63,35 +48,25 @@ describe BankAccount do
       bank_account.withdraw(500, '11/01/2023')
       bank_account.deposit(3000, '12/01/2023')
       bank_account.withdraw(2000, '13/01/2023')
-      expect(bank_account.get_transactions[0]).to include({date: '22/08/2022', value: 1000, balance: 1000})
-      expect(bank_account.get_transactions[1]).to include({date: '11/01/2023', value: -500, balance: 500})
-      expect(bank_account.get_transactions[2]).to include({date: '12/01/2023', value: 3000, balance: 3500})
-      expect(bank_account.get_transactions[3]).to include({date: '13/01/2023', value: -2000, balance: 1500})
-    end
-
-    it "updates the balance" do
-      io = double :io
-      bank_account = BankAccount.new(io)
-      bank_account.deposit(1000)
-      bank_account.withdraw(500)
-      bank_account.deposit(3000)
-      bank_account.withdraw(2000)
-      expect(bank_account.get_balance).to eq 1500
+      expect(bank_account.transactions[0]).to include({ date: '22/08/2022', value: 1000, balance: 1000 })
+      expect(bank_account.transactions[1]).to include({ date: '11/01/2023', value: -500, balance: 500 })
+      expect(bank_account.transactions[2]).to include({ date: '12/01/2023', value: 3000, balance: 3500 })
+      expect(bank_account.transactions[3]).to include({ date: '13/01/2023', value: -2000, balance: 1500 })
     end
   end
 
-  context "when priting a bank statement" do
-    it "provides a list of transactions with updates balance sorted by date in reverse order" do
+  context 'when priting a bank statement' do
+    it 'provides a list of transactions with updates balance sorted by date in reverse order' do
       io = double :io
-      expect(io).to receive(:puts).with("date || credit || debit || balance").ordered
-      expect(io).to receive(:puts).with("14/01/2023 || || 500.00 || 2500.00").ordered
-      expect(io).to receive(:puts).with("13/01/2023 || 2000.00 || || 3000.00").ordered
-      expect(io).to receive(:puts).with("10/01/2023 || 1000.00 || || 1000.00").ordered
+      expect(io).to receive(:puts).with('date || credit || debit || balance').ordered
+      expect(io).to receive(:puts).with('14/01/2023 || || 500.00 || 2500.00').ordered
+      expect(io).to receive(:puts).with('13/01/2023 || 2000.00 || || 3000.00').ordered
+      expect(io).to receive(:puts).with('10/01/2023 || 1000.00 || || 1000.00').ordered
       bank_account = BankAccount.new(io)
       bank_account.deposit(1000, '10/01/2023')
       bank_account.deposit(2000, '13/01/2023')
       bank_account.withdraw(500, '14/01/2023')
-      bank_account.get_bank_statement
+      bank_account.bank_statement
     end
   end
 end
