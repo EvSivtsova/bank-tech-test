be185f0134623ff7ff6de62b9e1c8ed0aa63d110
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-21 11:02:39 +0100

    Add test for instance - InputValidation - unit

--- spec/unit_tests/input_validation_spec.rb
+++ spec/unit_tests/input_validation_spec.rb
@@ -5,6 +5,10 @@ describe InputValidation do
     @input_validation = InputValidation.new
   end
 
+  it 'is an instance of InputValidation class' do
+    expect(@input_validation).to be_instance_of(InputValidation)
+  end
+
   it 'accepts Integers and floats as transaction values and correctly formatted dates' do
     expect { @input_validation.input_valid?(1000, '17/01/2023') }.to_not raise_error
     expect { @input_validation.input_valid?(1000.55, '17/01/2023') }.to_not raise_error
